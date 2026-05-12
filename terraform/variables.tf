933836786d946ef83501131b783688729c99e6ed
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-05-12 18:47:54 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,38 @@
+variable "aws_region" {
+
+  description = "AWS region"
+
+  type = string
+
+  default = "us-west-2"
+}
+
 variable "cluster_name" {
-  default = "sejal-demo-eks"
+
+  description = "EKS Cluster Name"
+
+  type = string
+
+  default = "devops-cluster"
 }
 
 variable "vpc_cidr" {
+
+  description = "VPC CIDR"
+
+  type = string
+
   default = "10.0.0.0/16"
 }
 
 variable "subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  description = "Subnet CIDRs"
+
+  type = list(string)
+
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
 }
