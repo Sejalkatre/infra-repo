2a656e833f4acec010a7cfe3b1820494d7315e89
Sejal Katre <96768363+Sejalkatre@users.noreply.github.com>
2026-05-12 18:38:45 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,8 @@
 terraform {
-
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
-
     aws = {
-
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
@@ -13,6 +10,5 @@ terraform {
 }
 
 provider "aws" {
-
-  region = "us-west-2"
+  region = var.aws_region
 }
